6c8ef49fdbf93650dde99e312de6fafd87d257c1
git config --global user.name your_username <dallen91801@gmail.com>
2024-10-06 18:27:32 -0700

    Initial commit for the sql-challenge

---
+++ sql_challenge_Final_table_schemata.sql
@@ -0,0 +1,86 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/cOyabc
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" INTEGER   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     ),
+    CONSTRAINT "uc_departments_dept_name" UNIQUE (
+        "dept_name"
+    )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "emp_title" VARCHAR(50)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    "INDEX" (first_name,lastname)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(50)   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "Index" (emp_no,title)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     ),
+    CONSTRAINT "uc_titles_title" UNIQUE (
+        "title"
+    )
+);
+
+CREATE TABLE "depart_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" INTEGER   NOT NULL,
+    "INDEX(emp_no," dept_no)   NOT NULL,
+    CONSTRAINT "pk_depart_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "depart_manager" (
+    "dept_no" INTEGER   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "INDEX" (dept_no,emp_no)   NOT NULL,
+    CONSTRAINT "pk_depart_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "titles" ("title");
+
+ALTER TABLE "depart_emp" ADD CONSTRAINT "fk_depart_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "depart_emp" ADD CONSTRAINT "fk_depart_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "depart_manager" ADD CONSTRAINT "fk_depart_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "depart_manager" ADD CONSTRAINT "fk_depart_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
